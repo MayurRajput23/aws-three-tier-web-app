ac96dab0dbfe9359f9dc03f9dcf0e3af7aa6496b
MayurRajput23 <mayur.rajput2505@gmail.com>
2025-03-18 10:16:13 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
+    DB_HOST : 'database-1.ctsckc2iwpmu.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'M786*687*m',
+    DB_DATABASE : 'webappdb'
 });
